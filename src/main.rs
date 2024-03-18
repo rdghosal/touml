6248978083291ac445e7c546f35ab5bbd64a8906
Rahul Ghosal <rdghosal@gmail.com>
2024-03-18 07:10:16 -0400

    wip mermaid mapper trait impl

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,108 @@
 use rustpython_parser::{ast, Parse};
+use std::collections::HashSet;
+use std::env;
 use std::error::Error;
 use std::path::PathBuf;
+use std::process::Command;
 use std::sync::{Arc, Mutex};
 use std::{fs, thread};
 
 const FILE_EXTS: [&str; 1] = ["py"];
+
+#[derive(PartialEq, Eq, Hash)]
+enum AccessLevel {
+    Public,
+    Private,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct MermaidMethod {
+    name: String,
+    access: AccessLevel,
+    args: Vec<String>,
+    returns: Option<String>,
+}
+
+struct MermaidField(String, AccessLevel);
+
+struct MermaidClass {
+    name: String,
+    access: AccessLevel,
+    parents: HashSet<Box<MermaidClass>>, // Maybe create an Rc on the parent struct?
+    methods: HashSet<MermaidMethod>,
+    fields: HashSet<MermaidField>,
+}
+
+trait MermaidMapper {
+    // TODO fix type
+    fn to_mermaid(self) -> Result<(), Box<dyn Error>>;
+}
+
+impl MermaidMapper for ast::StmtClassDef {
+    fn to_mermaid(self) -> Result<(), Box<dyn Error>> {
+        let name = self.name.to_string();
+        let mut parents = HashSet::new();
+        let mut methods = HashSet::new();
+        // let mut fields = HashSet::new();
+        for parent in self.bases {
+            match parent {
+                ast::Expr::Name(name) => {
+                    parents.insert(name.id.to_string());
+                }
+                ast::Expr::Attribute(attr) => {
+                    if let ast::Expr::Name(name) = attr.value.as_ref() {
+                        parents.insert(name.id.to_string());
+                    } else {
+                        // TODO: return error here instead of printing
+                        eprintln!("Failed to extract base class name from attribute.");
+                    }
+                }
+                _ => todo!(),
+            }
+        }
+        for attr in self.body {
+            match attr {
+                ast::Stmt::FunctionDef(func) => {
+                    let name = func.name.to_string();
+                    let mut args = Vec::new();
+                    args.extend(func.args.posonlyargs);
+                    args.extend(func.args.kwonlyargs);
+                    args.extend(func.args.args);
+                    let args = args.iter().map(|a| a.def.arg.to_string()).collect();
+                    let returns = if func.returns.is_some() {
+                        match *func.returns.unwrap() {
+                            ast::Expr::Constant(c) => {
+                                Some(String::new())
+                                // todo!("Match constant as ast::Expr::Constant")
+                                // constant.value
+                            }
+                            ast::Expr::Attribute(a) => Some(a.attr.to_string()),
+                            ast::Expr::Name(n) => Some(n.id.to_string()),
+                            _ => todo!(),
+                        }
+                    } else {
+                        None
+                    };
+
+                    let access = if name.starts_with("_") {
+                        AccessLevel::Private
+                    } else {
+                        AccessLevel::Public
+                    };
+
+                    methods.insert(MermaidMethod {
+                        name,
+                        access,
+                        args,
+                        returns,
+                    });
+                }
+                _ => todo!(),
+            }
+        }
+        Ok(())
+    }
+}
 
 fn set_file_paths(file_paths: &mut Vec<PathBuf>, curr_path: PathBuf) -> Result<(), Box<dyn Error>> {
     if curr_path.is_dir() {
@@ -49,5 +147,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let _ = handle.join().unwrap();
     }
     println!("Result: {:#?}", ast_nodes.lock().unwrap());
+    // dbg!("{#?}", env::consts::OS);
+    // Command::new("open").arg("src/index.html").spawn().unwrap();
     Ok(())
 }
--- tests/inputs/python/test_pkg/core/utils.py
+++ tests/inputs/python/test_pkg/core/utils.py
@@ -1,6 +1,29 @@
 __all__ = ("do_something",)
 
+import enum
+from typing import ClassVar, Any
+
 from ..models import StrClass, IntClass
+
+
+class ClassicClass:
+    A_CLS_VAR = 0
+    ANOTHER_CLS_VAR: ClassVar[list[dict[str, Any]]] = [
+        {
+            "a_key": 1,
+            "a_nested_key": {
+                1: 2,
+                3: 4,
+            },
+        }
+    ]
+
+    class Status(enum.Enum):
+        STATUS_1 = 1
+        STATUS_2 = 2
+
+    def __init__(self, val) -> None:
+        self.value = val
 
 
 def do_something() -> None:
