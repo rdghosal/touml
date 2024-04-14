4b24ed5877b4eea31f37ea5583775546bdd3fbdf
Rahul Ghosal <rdghosal@gmail.com>
2024-04-14 08:47:20 -0400

    refac: add anyhow, update path getter and main to use rayon against file parsing instead of concurrent file i/o

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use anyhow::Result;
+use rayon::prelude::*;
 use rustpython_parser::{ast, Parse};
+
 use std::collections::HashSet;
 use std::env;
 use std::error::Error;
+use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
-use std::sync::{Arc, Mutex};
-use std::{fs, thread};
 
 const FILE_EXTS: [&str; 1] = ["py"];
 
@@ -104,19 +106,19 @@ impl MermaidMapper for ast::StmtClassDef {
     }
 }
 
-fn set_file_paths(file_paths: &mut Vec<PathBuf>, curr_path: PathBuf) -> Result<(), Box<dyn Error>> {
-    if curr_path.is_dir() {
-        for entry in curr_path.read_dir()? {
+fn get_file_paths(root: PathBuf) -> Result<Vec<PathBuf>> {
+    let mut result = Vec::<PathBuf>::new();
+    if root.is_dir() {
+        for entry in root.read_dir()? {
             if let Ok(e) = entry {
                 let path = e.path();
                 if path.is_dir() {
-                    set_file_paths(file_paths, path.clone())?;
-                }
-                if let Some(ext) = path.extension() {
+                    result.append(&mut get_file_paths(path)?);
+                } else if let Some(ext) = path.extension() {
                     match ext.to_str() {
                         Some(e) => {
                             if FILE_EXTS.contains(&e) {
-                                file_paths.push(path);
+                                result.push(path);
                             }
                         }
                         None => (),
@@ -125,28 +127,24 @@ fn set_file_paths(file_paths: &mut Vec<PathBuf>, curr_path: PathBuf) -> Result<(
             }
         }
     }
-    Ok(())
+    Ok(result)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut paths = Vec::new();
-    set_file_paths(&mut paths, PathBuf::from("./tests/inputs/python"))?;
-    let ast_nodes = Arc::new(Mutex::new(Vec::<ast::Stmt>::new()));
-    let mut handles = Vec::new();
-    for path in paths {
-        let nodes = Arc::clone(&ast_nodes);
-        let handle = thread::spawn(move || -> Result<(), std::io::Error> {
-            let mut n = nodes.lock().unwrap();
-            let contents = fs::read_to_string(&path)?;
-            n.append(&mut ast::Suite::parse(&contents, path.to_str().unwrap()).unwrap());
-            Ok(())
-        });
-        handles.push(handle);
-    }
-    for handle in handles {
-        let _ = handle.join().unwrap();
-    }
-    println!("Result: {:#?}", ast_nodes.lock().unwrap());
+fn main() -> Result<()> {
+    let paths = get_file_paths(PathBuf::from("./tests/inputs/python"))?;
+    let ast = paths
+        .iter()
+        .filter_map(|p| match fs::read_to_string(p) {
+            Ok(c) => Some(c),
+            Err(_) => {
+                eprintln!("Failed to load contents from file {}.", p.to_string_lossy());
+                None
+            }
+        })
+        .par_bridge()
+        .map(|c| ast::Suite::parse(&c, "path").unwrap())
+        .collect::<Vec<_>>();
+    println!("Result: {:#?}", ast);
     // dbg!("{#?}", env::consts::OS);
     // Command::new("open").arg("src/index.html").spawn().unwrap();
     Ok(())
