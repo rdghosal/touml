b05e1ea0efe12e9b2e0a420c524d9ecacf77bdc1
Rahul Ghosal <rdghosal@gmail.com>
2024-04-29 08:54:30 -0400

    wip impl Display for MermaidClass and class diagram builder

--- src/generators/mermaid.rs
+++ src/generators/mermaid.rs
@@ -17,79 +17,70 @@ pub trait MermaidMappable {
     fn as_mermaid(self) -> MermaidClass;
 }
 
-// fn make_class_methods(cls: &MermaidClass) -> Vec<String> {
-//     let mut result = Vec::with_capacity(cls.methods.len());
-//     for method in &cls.methods {
-//         let access_modifier = match method.access {
-//             Accessibility::Public => '+',
-//             Accessibility::Private => '-',
-//         };
+impl MermaidClass {
+    fn make_class_methods(&self) -> Vec<String> {
+        let mut result = Vec::with_capacity(self.methods.len());
+        for method in &self.methods {
+            let access_modifier = match method.access {
+                Accessibility::Public => '+',
+                Accessibility::Private => '-',
+            };
+            let mut method_str = format!("{INDENT}{INDENT}{access_modifier}{}(", method.name);
+            let mut args: Vec<String> = vec![];
+            for arg in method.args.iter() {
+                if let Some(t) = &arg.dtype {
+                    args.push(format!("{t} {}", arg.name));
+                } else {
+                    args.push(arg.name.to_owned());
+                }
+            }
+            if args.len() > 0 {
+                let args_str = args.join(", ");
+                method_str.push_str(args_str.as_str());
+            }
+            method_str.push_str(")");
+            if let Some(return_type) = &method.returns {
+                method_str.push_str(&format!(" {return_type}"));
+            }
+            result.push(method_str);
+        }
+        result.push(format!("{}}}", INDENT));
+        result
+    }
 
-//         let mut method_str = format!("{INDENT}{INDENT}{access_modifier}{}(", method.name);
-//         let mut args: Vec<String> = vec![];
-//         for arg in method.args.iter() {
-//             if let Some(t) = &arg.dtype {
-//                 args.push(format!("{t} {}", arg.name));
-//             } else {
-//                 args.push(arg.name.to_owned());
-//             }
-//         }
-//         if args.len() > 0 {
-//             let args_str = args.join(", ");
-//             method_str.push_str(args_str.as_str());
-//         }
-//         method_str.push_str(")");
-//         if let Some(return_type) = &method.returns {
-//             method_str.push_str(&format!(" {return_type}"));
-//         }
-//         result.push(method_str);
-//     }
-//     result.push(format!("{}}}", INDENT));
-//     result
-// }
+    fn make_class_fields(&self) -> Vec<String> {
+        let mut result = Vec::with_capacity(self.fields.len());
+        for field in self.fields.iter() {
+            let line = match (&field.dtype, &field.default) {
+                (Some(t), _) => {
+                    format!("{INDENT}{INDENT}+{} {t}", field.name)
+                }
+                (_, Some(d)) => {
+                    format!("{INDENT}{INDENT}+{} = {d}", field.name)
+                }
+                _ => format!("{INDENT}{INDENT}+{}", field.name),
+            };
+            result.push(line);
+        }
+        result
+    }
+}
 
-// fn make_class_fields(model: &PyClass) -> Vec<String> {
-//     let mut result = Vec::with_capacity(model.fields.len());
-//     for field in model.fields.iter() {
-//         let line = match (&field.dtype, &field.default) {
-//             (Some(t), _) => {
-//                 format!("{INDENT}{INDENT}+{} {t}", field.name)
-//             }
-//             (_, Some(d)) => {
-//                 format!("{INDENT}{INDENT}+{} = {d}", field.name)
-//             }
-//             _ => format!("{INDENT}{INDENT}+{}", field.name),
-//         };
-//         result.push(line);
-//     }
-//     result
-// }
-
-// impl Display for MermaidClass {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//                 let mut result = vec![];
-//         let inherits = " <|-- ";
-//         for model in models.iter() {
-//             // Define class as well as the fields and methods therein.
-//             result.push(String::from("classDiagram"));
-
-//             // Define class as well as the fields and methods therein.
-//             let class_name = format!("{INDENT}class {}{{", model.name);
-//             result.push(class_name);
-//             result.extend(Self::make_class_fields(model));
-//             result.extend(Self::make_class_methods(model));
-//             for parent in model.parents.iter() {
-//                 result.push(format!("{INDENT}`{parent}`{inherits}{}", model.name));
-//             }
-//         }
-//         Ok(result.join("\r\n"))
-
-//         return Some(
-
-//         )
-
-//     }
-// }
+impl Display for MermaidClass {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut result = vec![];
+        let inherits = " <|-- ";
+        // Define class as well as the fields and methods therein.
+        let class_name = format!("{INDENT}class {}{{", self.name);
+        result.push(class_name);
+        result.extend(self.make_class_fields());
+        result.extend(self.make_class_methods());
+        for parent in self.parents.iter() {
+            result.push(format!("{INDENT}`{parent}`{inherits}{}", self.name));
+        }
+        write!(f, "{}", result.join("\r\n"))
+    }
+}
 
 impl MermaidMappable for PyClass {
     fn as_mermaid(self) -> MermaidClass {
@@ -114,6 +105,10 @@ impl MermaidMappable for PyClass {
     }
 }
 
-fn make_class_diagram(nodes: Vec<&dyn MermaidMappable>) -> String {
-    todo!()
+pub fn make_class_diagram(nodes: Vec<impl MermaidMappable>) -> String {
+    let mut result = vec![String::from("classDiagram")];
+    for node in nodes.into_iter() {
+        result.push(node.as_mermaid().to_string());
+    }
+    result.join("")
 }
