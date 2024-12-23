aa39f002de74ffef586c5c859b4e44d6c29a3f1c
Rahul Ghosal <rdghosal@proton.me>
2024-12-23 17:06:31 -0500

    refactor: var names

--- crates/touml/src/lib.rs
+++ crates/touml/src/lib.rs
@@ -16,11 +16,11 @@ pub fn python_to_mermaid(src: String, exclude_names: &[String]) -> Result<Option
     let result = python::PyClassInfo::from_source(&src)
         .map_err(|e| e.to_string())?
         .filter_map(|c| {
-            if let Ok(cc) = c {
-                if exclude_patterns.iter().any(|p| p.matches(&cc.name)) {
+            if let Ok(c) = c {
+                if exclude_patterns.iter().any(|p| p.matches(&c.name)) {
                     None
                 } else {
-                    Some(cc.to_mermaid().print())
+                    Some(c.to_mermaid().print())
                 }
             } else {
                 None
